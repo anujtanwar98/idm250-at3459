71b6428fe19b935c42f10a2d696512dcb6717e47
Anuj <anujtanwar89@gmail.com>
2023-03-12 21:13:02 -0400

    update single php

--- wp-content/themes/idm250-theme-01/single.php
+++ wp-content/themes/idm250-theme-01/single.php
@@ -2,11 +2,15 @@
     get_header();
 ?>
 <h1 class="single-page-title"><?php echo get_the_title(); ?></h1>
-<div><?php echo get_the_excerpt(); ?></div>
-<!-- <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div>
+  <?php 
+  // echo get_the_excerpt(); 
+  ?>
+</div>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <?php get_template_part('components/content'); ?>
 </article>
-<br> -->
+<br>
 <!-- <h2>Categories for this post</h2> -->
 <?php
 // $currentPostId = get_the_id();
