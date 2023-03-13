70dc4161be5723a23bf7cb36d59a5c8e421719f8
Anuj <anujtanwar89@gmail.com>
2023-03-13 00:11:20 -0400

    updates single shoes

--- wp-content/themes/idm250-theme-01/single-shoes.php
+++ wp-content/themes/idm250-theme-01/single-shoes.php
@@ -1,7 +1,11 @@
 <?php
     get_header();
 ?>
-Custom Post Type = Shoes
+<!-- Custom Post Type = Shoes -->
+<h1 class="single-page-title"><?php echo get_the_title(); ?></h1>
+  <div class="my-single-shoes-images-div">
+    <img class="my-single-shoes-images" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php echo get_the_title(); ?> Featured Image">
+  </div>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <?php get_template_part('components/content'); ?>
 </article>
