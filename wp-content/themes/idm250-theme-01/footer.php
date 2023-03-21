6c7686debd1ef59915bba7b87ed0a754872388b8
Anuj <anujtanwar89@gmail.com>
2023-03-21 14:49:06 -0400

    footer acf

--- wp-content/themes/idm250-theme-01/footer.php
+++ wp-content/themes/idm250-theme-01/footer.php
@@ -1,9 +1,10 @@
 </div>
     <footer class="main-footer">
         <p class="footer-copyright text-center py-3 mb-0 text-white">
-        &copy; The Shoe Stitcher <?php echo date("Y"); ?>
+        
+        &copy; <?php the_field('footer_text', 'option'); ?> <?php echo date("Y"); ?>
             <br>
-            Only for Educational purposes only
+            <?php the_field('footer_subtitle', 'option'); ?>
         </p>
     </footer>
     <?php // https://developer.wordpress.org/reference/functions/wp_footer/?>
