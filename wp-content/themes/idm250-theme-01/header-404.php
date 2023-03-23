2aaa3d7b51d760e9f921e3dd9e044ff6e4dac8d6
Anuj <anujtanwar89@gmail.com>
2023-03-23 16:17:28 -0400

    404 update

--- wp-content/themes/idm250-theme-01/header-404.php
+++ wp-content/themes/idm250-theme-01/header-404.php
@@ -39,18 +39,5 @@ $menu = get_theme_menu('primary-menu');
   
 
   <div class="main-content">
-  <div class="container my-error-container">
-    <div class="row">
-      <div class="col-md-6 offset-md-3 text-center">
-        <?php 
-        $image = get_field('404NotFound', 'option');
-        if( !empty( $image ) ): ?>
-          <img class="not-found" src="<?php echo esc_url($image['url']); ?>" class="img-fluid" alt="<?php echo esc_attr($image['alt']); ?>" />
-        <?php endif; ?>
-        <h1 class="error-title"><?php the_field('404_text_title', 'option'); ?></h1>
-        <p class="error-sub"><?php the_field('404_text_subtitle', 'option'); ?></p>
-        <a href="<?php echo home_url(); ?>" class="btn btn-primary my-404-home"><?php the_field('404_text_button', 'option'); ?></a>
-      </div>
-    </div>
-  </div>
+  
   
